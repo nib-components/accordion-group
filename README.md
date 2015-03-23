23c26de0ccfb7f8e96d38798fc751542f01bbaf0
James Newell <j.newell@nib.com.au>
2015-03-23 15:20:06 +1100

    use a separate trigger and wait for the previous accordion to close before opening

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 accordion-group
 ===============
 
-Show only one accordion body at a time within an accordion-group.
+A group of accordions where only one accordion can be open at any one time.
 
 Used in conjunction with [nib-components/accordion](https://github.com/nib-components/accordion) component.
 
@@ -19,19 +19,26 @@ Used in conjunction with [nib-components/accordion](https://github.com/nib-compo
 ## Example markup
 
     <div class="js-accordion-group">
+
+        <button class="js-trigger">Show and hide the content</button>
         <div class="js-accordion is-closed">
             <div class="js-body">
                 Show and hide me
             </div>
         </div>
+
+        <button class="js-trigger">Show and hide the content</button>
         <div class="js-accordion is-closed">
             <div class="js-body">
                 Show and hide me
             </div>
         </div>
+
+        <button class="js-trigger">Show and hide the content</button>
         <div class="js-accordion is-closed">
             <div class="js-body">
                 Show and hide me
             </div>
         </div>
+
     </div>
--- index.js
+++ index.js
@@ -1,41 +1,89 @@
+var event = require('event');
+var closest = require('closest');
 var Accordion = require('accordion');
 
 /**
  * A group of accordions
- * @param options
  * @constructor
+ * @param   {Object}        options
+ * @param   {HTMLElement}   options.el                  The element
+ * @param   {string}        [options.triggerSelector]   The trigger element selectors
+ * @param   {string}        [options.accordionSelector] The accordion element selectors
  */
 function AccordionGroup(options) {
   var self = this;
 
-  this._accordions = [];
-  var accordionElements = document.querySelectorAll(options.selector || AccordionGroup.selector);
+  var groupElement      = options.el;
 
-  function closeOtherAccordions() {
-    var accordion = this;
-    self._accordions.forEach(function(otherAccordion) {
-      if (accordion !== otherAccordion) {
-        otherAccordion.close();
-      }
-    });
+  this.triggerSelector   = options.triggerSelector || '.js-trigger';
+  this.triggerElements   = groupElement.querySelectorAll(this.triggerSelector);
 
+  this.accordionSelector = options.accordionSelector || '.js-accordion';
+  this.accordionElements = groupElement.querySelectorAll(this.accordionSelector);
+
+  if (this.triggerElements.length !== this.accordionElements.length) {
+    throw new Error('There must be as many trigger elements as accordion elements.');
   }
 
-  for (var i=0; i<accordionElements.length; ++i) {
+  for (var i=0; i<this.triggerElements.length; ++i) {
+    event.bind(this.triggerElements[i], 'click', this.onTriggerClicked.bind(this));
+  }
 
-    var accordion = new Accordion({
-      el: accordionElements[i],
-      triggerEl: accordionElements[i]//TODO: make this customisable
-    });
-
-    accordion.on('opened', closeOtherAccordions);
-
-    this._accordions.push(accordion);
-
+  this._accordions  = [];
+  for (var i=0; i<this.accordionElements.length; ++i) {
+    this._accordions.push(new Accordion({el: this.accordionElements[i]}));
   }
 
 }
 
-AccordionGroup.selector = '.js-accordion';
+/**
+ * Open an accordion, making sure all the other accordions are closed
+ * @param {number} index The accordion index
+ */
+AccordionGroup.prototype.open = function(index) {
+  var self = this;
+
+  function open() {
+    self._accordions[index].toggle();
+  }
+
+  function close(i) {
+
+    //check if we've closed all the accordions
+    if (i>=self._accordions.length) {
+      return open();
+    }
+
+    //ignore the triggered accordion
+    if (i === index) {
+      return close(++i);
+    }
+
+    //close the accordion
+    self._accordions[i]
+      .once('closed', function() {
+        console.log('Closed', i);
+        close(++i);
+      })
+      .close()
+    ;
+
+  }
+
+  close(0);
+
+};
+
+/**
+ * @private
+ * @param   {Event} event
+ */
+AccordionGroup.prototype.onTriggerClicked = function(event) {
+
+  var triggerElement  = closest(event.target, this.triggerSelector, true, true);
+  var triggerIndex    = Array.prototype.indexOf.call(this.triggerElements, triggerElement);
+
+  this.open(triggerIndex);
+};
 
 module.exports = AccordionGroup;
